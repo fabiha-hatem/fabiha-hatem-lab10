bd651b31095e44c2681efb3ff3d136bc9a2bcb3c
Your Name <you@example.com>
2025-11-03 02:27:01 -0500

    removing skeleton comments

--- WordCounter.java
+++ WordCounter.java
@@ -9,10 +9,7 @@ import java.io.IOException;
 
 public class WordCounter {
     public static int processText(StringBuffer text, String stopword) throws TooSmallText, InvalidStopwordException {
-        // returns number of words until the stopword, not inclufing the stopword 
-        // if it's less then five words it will throw toosmall even if the stopword is fousn 
-        // text is args[0]
-        // stopwors is arrgs[0]
+       
         Pattern regex = Pattern.compile("[a-zA-Z_0-9']+");
         Matcher regexMatcher = regex.matcher(text); 
         int num = 0; 
@@ -49,8 +46,7 @@ public class WordCounter {
         
 
     public static StringBuffer processFile(String path) throws EmptyFileException {
-        // return StringBuffer holding all contents of the file 
-        // when you try to open the file and it doesnt exsist yo have to catch a filenotfundexception 
+      
         Scanner sc = new Scanner(System.in); 
         String curr = path; 
         StringBuffer contents = new StringBuffer(); 
@@ -89,12 +85,7 @@ public class WordCounter {
 
     }
 
-    // path is args[0] in main 
-    // you can name path to text if you want 
-    // prompt the user to input one and 2 
-    // create scanner 
-    // call system .in 
-    // save that variable
+    
 
     public static void main(String [] args){
         if (args.length == 0 ){
